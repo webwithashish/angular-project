fd4c9530e093f0b3bdcbfc5139903f7328715dd4
Ashish Dattatraya Latake <95896308+webwithashish@users.noreply.github.com>
2025-03-24 18:38:20 +0530

    Add files via upload

---
+++ registration/registration.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NgForm , } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  standalone: false,
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.css'
+})
+export class RegistrationComponent {
+
+  onSubmit(form: NgForm) {
+    
+  }
+}

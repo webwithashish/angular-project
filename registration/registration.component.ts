8369e96c0008f445b0bb2b4d5797ed418fe21c73
Ashish Dattatraya Latake <95896308+webwithashish@users.noreply.github.com>
2025-03-25 07:59:35 +0530

    Add files via upload

--- registration/registration.component.ts
+++ registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NgForm , } from '@angular/forms';
+import { NgForm, } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-registration',
@@ -11,5 +12,45 @@ export class RegistrationComponent {
 
   onSubmit(form: NgForm) {
     
+  }
+
+  previewUrl: any;
+  isDragging = false;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  handleFileInput(event: any) {
+    const file = event.target.files[0];
+    this.processFile(file);
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging = false;
+    const file = event.dataTransfer?.files[0];
+    if (file) this.processFile(file);
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging = true;
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging = false;
+  }
+
+  private processFile(file: File) {
+    if (!file.type.match('image.*')) {
+      alert('Only image files are allowed!');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.previewUrl = this.sanitizer.bypassSecurityTrustUrl(reader.result as string);
+    };
   }
 }
